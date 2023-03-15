bfbd969c8c1426fc77efe28d8002caec01af27a6
Augusto Fernandez <augusfernandez23@gmail.com>
2023-03-15 18:29:51 -0300

    Se reestructuró el código

--- index.js
+++ index.js
@@ -1,55 +1,103 @@
 class ProductManager {
-    #products = [];
-    title = 'producto prueba';
-    description = 'Este es un producto de prueba';
-    price = 200;
-    thumbnail = 'Sin imagen';
-    code = 'abc123';
-    stock = 25;
-    quantity = 1;
-    id = 0
+  constructor() {
+    this.products = [];
+    this.nextId = 1;
+  }
+  
+  addProduct({title, description, price, thumbnail, code, stock}) {
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      throw new Error('Todos los campos son obligatorios');
+    }
+    /*
+    VALIDACIONES
+    if (typeof title !== 'string' || title.trim().length === 0) {
+      throw new Error('Title field is required and must be a non-empty string');
+    }
+    
+    if (typeof description !== 'string' || description.trim().length === 0) {
+      throw new Error('Description field is required and must be a non-empty string');
+    }
+    
+    if (typeof price !== 'number' || isNaN(price) || price <= 0) {
+      throw new Error('Price field is required and must be a positive number');
+    }
+    
+    if (typeof thumbnail !== 'string' || thumbnail.trim().length === 0) {
+      throw new Error('Thumbnail field is required and must be a non-empty string');
+    }
+    
+    if (typeof code !== 'string' || code.trim().length === 0) {
+      throw new Error('Code field is required and must be a non-empty string');
+    }
+    
+    if (typeof stock !== 'number' || isNaN(stock) || stock < 0) {
+      throw new Error('Stock field is required and must be a non-negative number');
+    }
+    */
+    
+    const product = {
+      id: this.nextId,
+      title: title,
+      description: description,
+      price: price,
+      thumbnail: thumbnail,
+      code: code,
+      stock: stock
+    };
 
-    constructor(title, description, price, thumbnail, code, stock){
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock =stock;
+    const duplicateProduct = this.products.find(p => p.code === product.code);
+    if (duplicateProduct) {
+      throw new Error('Producto duplicado');
     }
-    addProduct(prod) {
-        if (!prod.title || !prod.description || !prod.price || !prod.img || !prod.code || !prod.stock) {
-            throw new Error("Todos los campos son obligatorios")
-        };
-        const validateCode = this.#products.find(item => item.code === prod.code);
-        if (validateCode) {
-          validateCode.quantity++;
-          throw new Error("Codigo repetido")
-        } else {
-          this.id = this.id + 1;
-          return this.#products.push({...prod, id: this.id});
-        }
-    }
-    getProducts(){
-        console.log(this.#products)
-    }
-    getProductById(id) {
-        const productFind = this.#products.find(prod => prod.id === id);
 
-        if (!productFind){
-            throw Error("Not Found");
-        }
-        return console.log(productFind);
+    this.products.push(product);
+    this.nextId++;
+  }
+  
+  getProductById(id) {
+    const product = this.products.find(p => p.id === id);
+    if (!product) {
+      throw new Error('Not Found');
     }
+    
+    return product;
+  }
+  
+  getProducts() {
+    return this.products;
+  }
 }
 
-const producto1 = new ProductManager('Producto1', 'Instancia de producto 1', 300, 'Sin Imangen', 'adc124', 5);
-const producto2 = new ProductManager('Producto2', 'Instancia de producto 2', 400, 'Sin Imangen', 'adc124', 9);
-const producto3 = new ProductManager('Producto3', 'Instancia de producto 3', 500, 'Sin Imangen', 'adc125');
-producto1.getProducts();
-producto1.addProduct(producto1);
-producto2.addProduct(producto2);
-producto3.addProduct(producto3);
-producto1.getProducts();
-producto1.getProductById(1);
-producto1.getProductById(2); 
+const manager = new ProductManager();
+const item1 = {
+    title: "producto prueba",
+    description: "Este es un producto prueba",
+    price:200,
+    thumbnail: "Sin imagen",
+    code: "abc123",
+    stock:25
+}
+const item2 = {
+  title: "producto prueba",
+  description: "Este es un producto prueba",
+  price:200,
+  thumbnail: "Sin imagen",
+  code: "abc123",
+  stock:25
+}
+const item3 = {
+  title: "producto prueba",
+  description: "Este es un producto prueba",
+  price:200,
+  thumbnail: "Sin imagen",
+  code: "abc123",
+}
+
+console.log(manager.getProducts());
+manager.addProduct(item1);
+console.log(manager.getProducts());
+manager.addProduct(item2);
+manager.addProduct(item3);
+console.log(manager.getProducts());
+
+
