8d874195d8158c01e49ff0643f81e98a375c3d71
Augusto Fernandez <augusfernandez23@gmail.com>
2023-03-17 18:27:58 -0300

    Se modificó el código

--- index.js
+++ index.js
@@ -1,42 +1,16 @@
 class ProductManager {
   constructor() {
     this.products = [];
-    this.nextId = 1;
+    this.id = 1;
   }
   
   addProduct({title, description, price, thumbnail, code, stock}) {
     if (!title || !description || !price || !thumbnail || !code || !stock) {
       throw new Error('Todos los campos son obligatorios');
     }
-    /*
-    VALIDACIONES
-    if (typeof title !== 'string' || title.trim().length === 0) {
-      throw new Error('Title field is required and must be a non-empty string');
-    }
-    
-    if (typeof description !== 'string' || description.trim().length === 0) {
-      throw new Error('Description field is required and must be a non-empty string');
-    }
-    
-    if (typeof price !== 'number' || isNaN(price) || price <= 0) {
-      throw new Error('Price field is required and must be a positive number');
-    }
-    
-    if (typeof thumbnail !== 'string' || thumbnail.trim().length === 0) {
-      throw new Error('Thumbnail field is required and must be a non-empty string');
-    }
-    
-    if (typeof code !== 'string' || code.trim().length === 0) {
-      throw new Error('Code field is required and must be a non-empty string');
-    }
-    
-    if (typeof stock !== 'number' || isNaN(stock) || stock < 0) {
-      throw new Error('Stock field is required and must be a non-negative number');
-    }
-    */
     
     const product = {
-      id: this.nextId,
+      id: this.id,
       title: title,
       description: description,
       price: price,
@@ -51,7 +25,7 @@ class ProductManager {
     }
 
     this.products.push(product);
-    this.nextId++;
+    this.id++;
   }
   
   getProductById(id) {
