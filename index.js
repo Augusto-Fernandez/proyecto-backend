94dba31ff475f474cc6260137b62c1dca2c9d79e
Augusto Fernandez <augusfernandez23@gmail.com>
2023-03-21 17:20:47 -0300

    Se agregaron los metodos updateProduct y deleteProduct

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const fs = require("fs").promises;
+
 class ProductManager {
+  id=1;
+  #products = [];
+  path = ``;
+
   constructor() {
-    this.products = [];
+    this.#products = [];
     this.id = 1;
+    this.path = `./products.json`
   }
   
-  addProduct({title, description, price, thumbnail, code, stock}) {
+  async addProduct({title, description, price, thumbnail, code, stock}) {
     
     if (typeof title !== 'string' || title.trim().length === 0) {
       return "Campo obligatorio";
@@ -40,26 +47,95 @@ class ProductManager {
       stock: stock
     };
 
-    const validateCode = this.products.find(p => p.code === product.code);
-    if (validateCode) {
-      return 'Producto duplicado';
-    }
+    try {
+      const productFile = await fs.readFile(this.path, "utf-8");
+      let newProduct = JSON.parse(productFile);
 
-    this.products.push(product);
-    this.id++;
+      const validateId = newProduct.find((p) => p.id === product.id);
+      const validateCode = newProduct.find((p) => p.code === product.code)
+
+      if (validateId) {
+        throw new Error("Id repetido");
+      }
+
+      if (validateCode) {
+        throw new Error("Code repetido");
+      }
+
+      if (newProduct.length > 0) {
+        const lastProduct = newProduct[newProduct.length - 1];
+        this.id = lastProduct.id + 1;
+      }
+
+      newProduct.push({id: this.id++, ...product});
+
+      await fs.writeFile(this.path, JSON.stringify(newProduct, null, 2));
+      return "El objeto se ha creado correctamente";
+    } catch(e) {
+      throw new Error(e);
+    }
   }
   
-  getProducts() {
-    return this.products;
+  async getProducts(){
+    try{
+      const productFile = await fs.readFile(this.path, "utf-8");
+      return JSON.parse(productFile);
+    }catch(e){
+      await fs.writeFile(this.path, "[]");
+      return "No existe el archivo. Se creo uno con un array vacio";
+    }
   }
 
-  getProductById(id) {
-    const product = this.products.find(p => p.id === id);
-    if (!product) {
-      return "Not Found";
+  async getProductById(id) {
+    try {
+      const productFile = await fs.readFile(this.path, "utf-8");
+      let idProducts = JSON.parse(productFile);
+
+      const product = idProducts.find(p => p.id === id);
+
+      if (!product) {
+        throw new Error("Not Found");
+      }
+      return product;
+    } catch (e) {
+      throw new Error(e);
     }
-    
-    return product;
+  }
+
+  async updateProduct(id, product) {
+    try {
+      const productFile = await fs.readFile(this.path, "utf-8");
+      let products = JSON.parse(productFile);
+
+      const idProduct = products.findIndex((p) => p.id === id);
+
+      products.splice(idProduct, 1, { id, ...product });
+
+      await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+
+      return `Se ha modificado el producto`;
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+  async deleteProduct(id) {
+    try {
+      const productFile = await fs.readFile(this.path, "utf-8");
+      let products = JSON.parse(productFile);
+
+      const idProduct = products.find((p) => p.id === id);
+
+      if (!idProduct) {
+        throw new Error("Id no encontrado");
+      }
+      const deletedProducts = products.filter((p) => p.id !== id);
+
+      await fs.writeFile(this.path, JSON.stringify(deletedProducts, null, 2));
+
+      return `Se ha eliminado el producto`;
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 }
 
@@ -72,29 +148,20 @@ const item1 = {
     code: "abc123",
     stock:25
 }
-const item2 = {
-  title: "producto prueba",
-  description: "Este es un producto prueba",
-  price:200,
-  thumbnail: "Sin imagen",
-  code: "abc123",
-  stock:25
-}
-const item3 = {
-  title: "producto prueba",
-  description: "Este es un producto prueba",
-  price:200,
-  thumbnail: "Sin imagen",
-  code: "abc123",
+
+const main = async () => {
+  console.log(await manager.getProducts());
+  console.log(await manager.addProduct(item1));
+  console.log(await manager.getProducts());
+  console.log(await manager.getProductById(1));
+  console.log(await manager.updateProduct(1, {...item1, title: "nuevo producto"}));
+  console.log(await manager.getProducts());
+  console.log(await manager.deleteProduct(1));
+  console.log(await manager.getProducts());
 }
 
-console.log(manager.getProducts());
-manager.addProduct(item1);
-console.log(manager.getProducts());
-console.log(manager.addProduct(item2))
-console.log(manager.addProduct(item3))
-console.log(manager.getProducts());
-console.log(manager.getProductById(1));
-console.log(manager.getProductById(2));
+main();
+
+
 
 
