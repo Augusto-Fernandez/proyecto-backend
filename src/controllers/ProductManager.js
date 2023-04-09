a365598ba263db7aadf13b967668ea9f36599bb7
Augusto Fernandez <augusfernandez23@gmail.com>
2023-04-09 16:12:53 -0300

    Se agregó manejo de errores en ProductManager

--- src/controllers/ProductManager.js
+++ src/controllers/ProductManager.js
@@ -13,34 +13,6 @@ class ProductManager {
   
   async addProduct({title, description, price, thumbnail, code, stock, status}) {
     
-    if (typeof title !== 'string' || title.trim().length === 0) {
-      return "Campo obligatorio";
-    }
-    
-    if (typeof description !== 'string' || description.trim().length === 0) {
-      return "Campo obligatorio";
-    }
-    
-    if (typeof price !== 'number' || price <= 0) {
-      return "Campo obligatorio";
-    }
-    
-    if (typeof thumbnail !== 'string' || thumbnail.trim().length === 0) {
-      return "Campo obligatorio";
-    }
-    
-    if (typeof code !== 'string' || code.trim().length === 0) {
-      return "Campo obligatorio";
-    }
-    
-    if (typeof stock !== 'number' || stock < 0) {
-      return "Campo obligatorio";
-    }
-
-    if (typeof status !== 'boolean'){
-      return "Campo obligatorio"; 
-    }
-    
     const product = {
       id: this.id,
       title: title,
@@ -56,17 +28,6 @@ class ProductManager {
       const productFile = await fs.readFile(this.path, "utf-8");
       let newProduct = JSON.parse(productFile);
 
-      const validateId = newProduct.find((p) => p.id === product.id);
-      const validateCode = newProduct.find((p) => p.code === product.code)
-
-      if (validateId) {
-        throw new Error("Id repetido");
-      }
-
-      if (validateCode) {
-        throw new Error("Code repetido");
-      }
-
       if (newProduct.length > 0) {
         const lastProduct = newProduct[newProduct.length - 1];
         this.id = lastProduct.id + 1;
@@ -77,7 +38,13 @@ class ProductManager {
       await fs.writeFile(this.path, JSON.stringify(newProduct, null, 2));
       return "El objeto se ha creado correctamente";
     } catch(e) {
-      throw new Error(e);
+      await fs.writeFile(this.path, "[]");
+      const productFile = await fs.readFile(this.path, "utf-8");
+      const newProduct = JSON.parse(productFile);
+      newProduct.push({id: this.id++, ...product});
+
+      await fs.writeFile(this.path, JSON.stringify(newProduct, null, 2));
+      return "El objeto se ha creado correctamente";
     }
   }
   
@@ -92,55 +59,39 @@ class ProductManager {
   }
 
   async getProductById(id) {
-    try {
-      const productFile = await fs.readFile(this.path, "utf-8");
-      let idProducts = JSON.parse(productFile);
+    const productFile = await fs.readFile(this.path, "utf-8");
+    let idProducts = JSON.parse(productFile);
 
-      const product = idProducts.find(p => p.id === id);
+    const product = idProducts.find(p => p.id === id);
 
-      if (!product) {
-        throw new Error("Not Found");
-      }
-      return product;
-    } catch (e) {
-      throw new Error(e);
+    if (!product) {
+      return null;
     }
+    return product;
   }
 
   async updateProduct(id, product) {
-    try {
-      const productFile = await fs.readFile(this.path, "utf-8");
-      let products = JSON.parse(productFile);
+    const productFile = await fs.readFile(this.path, "utf-8");
+    let products = JSON.parse(productFile);
 
-      const idProduct = products.findIndex((p) => p.id === id);
+    const idProduct = products.findIndex((p) => p.id === id);
 
-      products.splice(idProduct, 1, { id, ...product });
+    products.splice(idProduct, 1, { id, ...product });
 
-      await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+    await fs.writeFile(this.path, JSON.stringify(products, null, 2));
 
-      return `Se ha modificado el producto`;
-    } catch (e) {
-      throw new Error(e);
-    }
+    return `Se ha modificado el producto`;
   }
+  
   async deleteProduct(id) {
-    try {
-      const productFile = await fs.readFile(this.path, "utf-8");
-      let products = JSON.parse(productFile);
+    const productFile = await fs.readFile(this.path, "utf-8");
+    let products = JSON.parse(productFile);
 
-      const idProduct = products.find((p) => p.id === id);
+    const deletedProducts = products.filter((p) => p.id !== id);
 
-      if (!idProduct) {
-        throw new Error("Id no encontrado");
-      }
-      const deletedProducts = products.filter((p) => p.id !== id);
+    await fs.writeFile(this.path, JSON.stringify(deletedProducts, null, 2));
 
-      await fs.writeFile(this.path, JSON.stringify(deletedProducts, null, 2));
-
-      return `Se ha eliminado el producto`;
-    } catch (e) {
-      throw new Error(e);
-    }
+    return `Se ha eliminado el producto`;
   }
 }
 
--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -35,14 +35,7 @@ const item3 = {
     status: true
 }
 
-const main = async () => {
-    await manager.getProducts();
-    await manager.addProduct(item1);
-    await manager.addProduct(item2);
-    await manager.addProduct(item3);
-}
 
-main();
 
 productRouter.get("/", async (req, res) => {
     const productsArray = await manager.getProducts();
@@ -59,9 +52,11 @@ productRouter.get("/", async (req, res) => {
 
 productRouter.get("/:pid", async (req, res) => {
     const productId = +req.params.pid;
-    const productsArray = await manager.getProducts()
+    const productById = await manager.getProductById(productId)
 
-    if(typeof productId !== 'number' || productId === 0 || isNaN(productId) || productId>productsArray.length){
+    if(typeof productId !== 'number' || productId === 0 || isNaN(productId)){
+        res.send({error: "Id is not valid"});
+    }else if(!productById){
         res.send({error: "Id not found"});
     }else{
         const productById = await manager.getProductById(parseInt(productId));
@@ -71,36 +66,93 @@ productRouter.get("/:pid", async (req, res) => {
 
 productRouter.post("/", async (req, res) =>{
     let product = req.body;
+    const productsArray = await manager.getProducts();
 
-    if(!product.title || !product.description || !product.code || !product.price || !product.status || !product.stock || !product.thumbnail){
-        return res.status(400).send({status: "error", error: "Todos los campos son obligatorios"});
-    }else{
-        manager.addProduct(product);
-        return res.status(201).send({status: "sucess", message: "Item created"});
+    if (!product.title || typeof product.title !== 'string' || product.title.trim().length === 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
     }
+    if (!product.description || typeof product.description !== 'string' || product.description.trim().length === 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    } 
+    if (!product.price || typeof product.price !== 'number' || product.price <= 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    } 
+    if (!product.thumbnail || typeof product.thumbnail !== 'string' || product.thumbnail.trim().length === 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    } 
+    if (!product.code || typeof product.code !== 'string' || product.code.trim().length === 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    }  
+    if (!product.stock || typeof product.stock !== 'number' || product.stock < 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    }
+    if (!product.status || typeof product.status !== 'boolean'){
+        return res.status(400).send({status: "error", error: "Campo obligatorio"}); 
+    }
+
+    const validateId = productsArray.find((p) => p.id === product.id);
+    const validateCode = productsArray.find((p) => p.code === product.code)
+
+    if (validateId) {
+        return res.status(400).send({status: "error", error: "Id Repetido"})
+    }
+
+    if (validateCode) {
+        return res.status(400).send({status: "error", error: "Code Repetido"})
+    }
+
+    manager.addProduct(product);
+    return res.status(201).send({status: "sucess", message: "Item created"});
 })
 
 productRouter.put("/:pid", async (req, res) =>{
     let product = req.body;
     let productId = +req.params.pid;
     const productsArray = await manager.getProducts()
-
-    if(!product.title || !product.description || !product.code || !product.price || !product.status || !product.stock || !product.thumbnail){
-        return res.status(400).send({status: "error", error: "Todos los campos son obligatorios"});
-    }else if(typeof productId !== 'number' || productId === 0 || isNaN(productId) || productId>productsArray.length){
-        res.send({error: "Id not found"});
-    }else{
-        manager.updateProduct(productId, product)
-        return res.status(200).send({status: "sucess", message: "Item modified"});
+    const productsById = await manager.getProductById(productId)
+    
+    if(typeof productId !== 'number' || productId === 0 || isNaN(productId) || productId>productsArray.length){
+        return res.send({error: "Id not found"});
     }
+    if(!productsById){
+        return res.status(400).send({status: "error", error: "Product not found"});
+    }
+
+    if (!product.title || typeof product.title !== 'string' || product.title.trim().length === 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    }
+    if (!product.description || typeof product.description !== 'string' || product.description.trim().length === 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    } 
+    if (!product.price || typeof product.price !== 'number' || product.price <= 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    } 
+    if (!product.thumbnail || typeof product.thumbnail !== 'string' || product.thumbnail.trim().length === 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    } 
+    if (!product.code || typeof product.code !== 'string' || product.code.trim().length === 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    }  
+    if (!product.stock || typeof product.stock !== 'number' || product.stock < 0) {
+        return res.status(400).send({status: "error", error: "Campo obligatorio"});
+    }
+    if (!product.status || typeof product.status !== 'boolean'){
+        return res.status(400).send({status: "error", error: "Campo obligatorio"}); 
+    }
+
+    manager.updateProduct(productId, product)
+    return res.status(200).send({status: "sucess", message: "Item modified"});
 })
 
 productRouter.delete("/:pid", async (req, res) => {
     const productId = +req.params.pid;
     const productsArray = await manager.getProducts()
+    const productsById = await manager.getProductById(productId)
 
     if(typeof productId !== 'number' || productId === 0 || isNaN(productId) || productId>productsArray.length){
-        res.send({error: "Id not found"});
+        res.send({error: "Id not valid"});
+    }else if(!productsById){
+        return res.status(400).send({status: "error", error: "Product not found"});
     }else{
         manager.deleteProduct(productId);
         return res.status(200).send({status: "sucess", message: "Item deleted"});
