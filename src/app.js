817629c8d8e681fcc47a00de6c74a0a277260080
Augusto Fernandez <augusfernandez23@gmail.com>
2023-04-11 20:30:33 -0300

    Se agregaron layouts

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 import express from "express";
 import productRouter from "./routes/ProductRouter.js";
 import cartRouter from "./routes/CartRouter.js";
+import {engine} from "express-handlebars";
+import {resolve} from "path";
+import ProductManager from "./controllers/ProductManager.js";
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+
+const viewPath = resolve('views');
+
+app.engine('handlebars', engine({
+    layoutsDir: `${viewPath}/layouts`,
+    defaultLayout: `${viewPath}/layouts/main.handlebars`
+}))
+
+app.set('view engine', 'handlebars');
+app.set('views', viewPath);
+
+app.get('/',  async (req, res) => {
+    const manager = new ProductManager();
+    const productsArray = await manager.getProducts();
+    res.render('products', {title: 'Products', productsArray})
+})
 
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
