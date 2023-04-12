554264ab7b5b74f499acf0d17b5cee056219d826
Augusto Fernandez <augusfernandez23@gmail.com>
2023-04-12 16:47:14 -0300

    Se creó ruta realtimeproducts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import productRouter from "./routes/ProductRouter.js";
 import cartRouter from "./routes/CartRouter.js";
+import viewsRouter from "./routes/ViewsRouter.js";
 import {engine} from "express-handlebars";
 import {resolve} from "path";
 import ProductManager from "./controllers/ProductManager.js";
@@ -27,6 +28,7 @@ app.get('/',  async (req, res) => {
 
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
+app.use('/api/realtimeproducts', viewsRouter);
 
 app.listen(8080, () => {
     console.log('Servidor escuchando en el puerto 8080');
---
+++ src/routes/ViewsRouter.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import ProductManager from "../controllers/ProductManager.js";
+
+const viewsRouter = Router();
+
+viewsRouter.get("/", async (req, res) => {
+    const manager = new ProductManager();
+    const productsArray = await manager.getProducts();
+    res.render('realTimeProducts', {title: 'Products', productsArray})
+});
+
+export default viewsRouter
