212d4f2f85916197d89edc30fea0b980de8ef35c
Augusto Fernandez <augusfernandez23@gmail.com>
2023-05-05 19:27:58 -0300

    Se removió filesystem de ProductRouter y CartRouter

--- src/routes/CartRouter.js
+++ src/routes/CartRouter.js
@@ -1,70 +1,20 @@
 import { Router} from "express";
-import CartManager from "../controllers/CartManager.js";
-import ProductManager from "../controllers/ProductManager.js";
 import cartsSchema from "../models/cartsSchema.js";
-import productsSchema from "../models/productsSchema.js";
-
-const manager = new CartManager();
-const productManager = new ProductManager();
 
 const cartRouter = Router();
 
 cartRouter.post("/", async (req,res) =>{
-    /*
-    manager.createCart()
-    return res.status(201).send({status: "sucess", message: "Cart created"});
-    */
     const createCart = await cartsSchema.create({});
     res.status(201).send({status: "sucess", createCart, message: "Cart created"});
 })
 
 cartRouter.get("/:cid", async (req, res) =>{
-    /*
-    const cartId = +req.params.cid;
-    const cartById = await manager.getCartById(cartId);
-
-    if(typeof cartId !== 'number' || cartId === 0 || isNaN(cartId)){
-        return res.status(401).send({status: "error", error: "Id not valid"});
-    }else if(!cartById){
-        return res.status(401).send({status: "error", error: "Id not found"});
-    }else{
-        return res.send(cartById);
-    }
-    */
     const cartId = req.params.cid;
     const cartById = await cartsSchema.findOne({_id: cartId});
     res.send({status: 'success', cartById});
 })
 
 cartRouter.post("/:cid/products/:pid", async (req,res) =>{
-    /*
-    let cartProductId = +req.params.pid;
-    let cartId = +req.params.cid;
-    const productId = await productManager.getProductById(cartProductId)
-    const cartsById = await manager.getCartById(cartId);
-
-    if (typeof cartId !== 'number' || cartId <= 0  || isNaN(cartProductId)) {
-        return res.status(401).send({status: "error", error: "Campo obligatorio"});
-    }
-    if (typeof cartProductId !== 'number' || cartProductId <= 0 || isNaN(cartProductId)) {
-        return res.status(401).send({status: "error", error: "Campo obligatorio"});
-    }
-
-    if(!productId){
-        return res.status(401).send({status: "error", error: "Id not found"});
-    }else if(!cartsById){
-        return res.status(401).send({status: "error", error: "Cart not found"});
-    }else{
-        const cartById = await manager.addProductToCart(cartId, cartProductId);
-        return res.send(cartById);
-    }
-    */
-
-    /*
-    const cartsById = await cartsSchema.findOne({_id: cartId});
-    const productId = await productsSchema.findOne({_id: cartProductId})
-    */
-
     let cartId = req.params.cid;
     let cartProductId = req.params.pid;
 
--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,86 +1,14 @@
-import ProductManager from "../controllers/ProductManager.js";
 import { Router} from "express";
 import productsSchema from "../models/productsSchema.js";
 
 const productRouter = Router();
 
-/*
-const manager = new ProductManager();
-
-const item1 = {
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price:200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-    stock:25,
-    status: true
-}
-
-const item2 = {
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price:200,
-    thumbnail: "Sin imagen",
-    code: "abc124",
-    stock:25,
-    status: true
-}
-
-const item3 = {
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price:200,
-    thumbnail: "Sin imagen",
-    code: "abc125",
-    stock:25,
-    status: true
-}
-
-const main = async () => {
-    await manager.addProduct(item1);
-    await manager.addProduct(item2);
-    await manager.addProduct(item3);
-    
-    const createProduct1 = await productsSchema.create(item1)
-    const createProduct2 = await productsSchema.create(item2)
-    const createProduct3 = await productsSchema.create(item3)
-}
-main();
-*/
-
-
 productRouter.get("/", async (req, res) => {
-    /* 
-        const productsArray = await manager.getProducts();
-        let {limit} = req.query;
-        let limitInt = parseInt(limit)
-    
-        if(limitInt > 0 && limitInt <= productsArray.length){
-            const limitedProducts = productsArray.splice(0,limitInt);
-            return res.send(limitedProducts);
-        }else{
-            return res.send(productsArray);
-        }  
-    */
     const productsArray = await productsSchema.find();
     res.send({status: 'success', productsArray});
 });
 
 productRouter.get("/:pid", async (req, res) => {
-    /*  
-        const productId = +req.params.pid;
-        const productById = await manager.getProductById(parseInt(productId));
-
-        if(typeof productId !== 'number' || productId === 0 || isNaN(productId)){
-            return res.status(401).send({status: "error", error: "Id not valid"});
-        }else if(!productById){
-            return res.status(401).send({status: "error", error: "Id not found"});
-        }else{
-            return res.send(productById);
-        } 
-    */
-
     const productId = req.params.pid;
     const productById = await productsSchema.findOne({_id: productId})
 
@@ -88,46 +16,12 @@ productRouter.get("/:pid", async (req, res) => {
 });
 
 productRouter.post("/", async (req, res) =>{
-    /* 
-    let product = req.body;
-    const productsArray = await manager.getProducts();
-
-    const validateId = productsArray.find((p) => p.id === product.id);
-    const validateCode = productsArray.find((p) => p.code === product.code)
-
-    if(!product.title || !product.description || !product.code || !product.price || !product.status || !product.stock || !product.thumbnail){
-        return res.status(401).send({status: "error", error: "Todos los campos son obligatorios"});
-    }else if(validateId){
-        return res.status(401).send({status: "error", error: "Id Repetido"})
-    }else if(validateCode){
-        return res.status(401).send({status: "error", error: "Code Repetido"})
-    }else{
-        manager.addProduct(product);
-        return res.status(201).send({status: "sucess", message: "Item created"});
-    }
-    */
     let product = req.body;
     const addProduct = await productsSchema.create(product)
     res.status(201).send({status: "sucess", addProduct, message: "Item created"});
 })
 
 productRouter.put("/:pid", async (req, res) =>{
-    /* 
-    let product = req.body;
-    let productId = +req.params.pid;
-    const productById = await manager.getProductById(productId)
-
-    if(!product.title || !product.description || !product.code || !product.price || !product.status || !product.stock || !product.thumbnail){
-        return res.status(401).send({status: "error", error: "Todos los campos son obligatorios"});
-    }else if(typeof productId !== 'number' || productId === 0 || isNaN(productId)){
-        return res.status(401).send({status: "error", error: "Id not valid"});
-    }else if(!productById){
-        return res.status(401).send({status: "error", error: "Id not found"});
-    }else{
-        manager.updateProduct(productId, product)
-        return res.status(200).send({status: "sucess", message: "Item modified"});
-    }
-    */
     let product = req.body;
     let productId = req.params.pid;
     const productModified = await productsSchema.updateOne({_id: productId}, product)
@@ -135,20 +29,6 @@ productRouter.put("/:pid", async (req, res) =>{
 })
 
 productRouter.delete("/:pid", async (req, res) => {
-    /* 
-    const productId = +req.params.pid;
-    const productById = await manager.getProductById(productId)
-
-    if(typeof productId !== 'number' || productId === 0 || isNaN(productId)){
-        return res.status(401).send({status: "error", error: "Id not valid"});
-    }else if(!productById){
-        return res.status(401).send({status: "error", error: "Id not found"});
-    }else{
-        manager.deleteProduct(productId);
-        return res.status(200).send({status: "sucess", message: "Item deleted"});
-    }
-    */
-
     const productId = req.params.pid;
     const deleteProduct = await productsSchema.deleteOne({_id: productId})
     return res.status(200).send({status: "sucess", deleteProduct, message: "Item deleted"});
