9e0f3db80239d2ac8a3299f3ce0fa95209b41211
Augusto Fernandez <augusfernandez23@gmail.com>
2023-05-06 10:55:28 -0300

    Se agregaron validaciones en CartRouter

--- src/routes/CartRouter.js
+++ src/routes/CartRouter.js
@@ -1,5 +1,6 @@
 import { Router} from "express";
 import cartsSchema from "../models/cartsSchema.js";
+import productsSchema from "../models/productsSchema.js";
 
 const cartRouter = Router();
 
@@ -10,33 +11,51 @@ cartRouter.post("/", async (req,res) =>{
 
 cartRouter.get("/:cid", async (req, res) =>{
     const cartId = req.params.cid;
-    const cartById = await cartsSchema.findOne({_id: cartId});
-    res.send({status: 'success', cartById});
+    try{
+        const cartById = await cartsSchema.findOne({_id: cartId});
+        if(cartById === null){
+            return res.status(401).send({status: "error", error: "Id not found"});
+        }
+        res.send({status: 'success', cartById});
+    }catch{
+        return res.status(401).send({status: "error", error: "Id not found"});
+    }
 })
 
 cartRouter.post("/:cid/products/:pid", async (req,res) =>{
     let cartId = req.params.cid;
     let cartProductId = req.params.pid;
 
-    const cartById = await cartsSchema.findById(cartId);
-    const productExist = cartById.products.findIndex(product => product.id === cartProductId);
+    try{
+        const cartById = await cartsSchema.findById(cartId);
+        if(cartById === null){
+            return res.status(401).send({status: "error", error: "Id not found"});
+        }
+        const productById = await productsSchema.findById(cartProductId);
+        if(productById === null){
+            return res.status(401).send({status: "error", error: "Id not found"});
+        }
+        const productExist = cartById.products.findIndex(product => product.id === cartProductId);
 
-    if(productExist!==-1){
-        const update=cartById.products[productExist].quantity+1;
-        await cartsSchema.findOneAndUpdate(
-            {_id:cartId, "products.id":cartProductId},
-            {$set:{"products.$.quantity":update}}
-        );
-        const newCart=await cartsSchema.findById(cartId);
-        res.status(200).send({status:"sucess", newCart, message:"Product added",
+        if(productExist!==-1){
+            const update=cartById.products[productExist].quantity+1;
+            await cartsSchema.findOneAndUpdate(
+                {_id:cartId, "products.id":cartProductId},
+                {$set:{"products.$.quantity":update}}
+            );
+            const newCart=await cartsSchema.findById(cartId);
+            res.status(200).send({status:"sucess", newCart, message:"Product added",
         });
-    }else{
-        const addProductToCart = await cartsSchema.findByIdAndUpdate(
-            cartId,
-            {$push:{products:{id: cartProductId, quantity: 1}}},
-            {new: true}
-        )
+        }else{
+            const addProductToCart = await cartsSchema.findByIdAndUpdate(
+                cartId,
+                {$push:{products:{id: cartProductId, quantity: 1}}},
+                {new: true}
+        );
         res.status(201).send({status: "sucess", addProductToCart, message: "Product added"});
+        }
+    }catch{
+        return res.status(401).send({status: "error", error: "Id not found"});
     }
 })
 
