902cbe46c3edd280f4c65f12c5bc4d9e3c8b3493
Augusto Fernandez <augusfernandez23@gmail.com>
2023-06-15 15:36:52 -0300

    Se agregó appFactory y directorio config al proyecto

--- src/app.js
+++ src/app.js
@@ -1,86 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
-import {engine} from "express-handlebars";
-import {resolve} from "path";
-import { Server } from "socket.io";
 import mongoose from "mongoose";
-import session from "express-session";
-import cookieParser from "cookie-parser";
-import MongoStore from "connect-mongo";
 
-import ProductManager from "./domain/managers/ProductManager.js";
-import productRouter from "./presentation/routes/ProductRouter.js";
-import cartRouter from "./presentation/routes/CartRouter.js";
-import viewsRouter from "./presentation/routes/ViewsRouter.js";
-import sessionRouter from "./presentation/routes/SessionRouter.js";
-import userRouter from "./presentation/routes/UserRouter.js";
-import roleRouter from "./presentation/routes/RoleRouter.js";
-import errorHandler from "./presentation/middlewares/errorHandler.js";
+import AppFactory from "./presentation/factories/appFactory.js";
 
 await mongoose.connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
-const app = express();
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
-app.use(session({
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGO_DB_URI,
-        ttl: 300
-    }),
-    secret: 'S3cR3tC0D3',
-    resave: false,
-    saveUninitialized: false
-}))
+const app = AppFactory.create(process.env.APPLICATION)
 
-const manager = new ProductManager();
+app.init();
+app.build();
+app.listen();
 
-const viewPath = resolve('src/views'); /* resolve('views') para node */
-
-app.engine('handlebars', engine({
-    layoutsDir: `${viewPath}/layouts`,
-    defaultLayout: `${viewPath}/layouts/main.handlebars`
-}))
-
-app.set('view engine', 'handlebars');
-app.set('views', viewPath);
-
-app.get('/',  async (req, res) => {    
-    const productsArray = await manager.getProducts();
-    res.render('index', {title: 'Products', productsArray})
-})
-
-app.use('/api/products', productRouter);
-app.use('/api/carts', cartRouter);
-app.use('/api/realtimeproducts', viewsRouter);
-app.use('/api/sessions', sessionRouter);
-app.use('/api/users', userRouter);
-app.use('/api/roles', roleRouter)
-app.use(errorHandler);
-
-const httpServer = app.listen(process.env.NODE_PORT, () => {
-    console.log(`Server listening on port ${process.env.NODE_PORT}`);
-});
-
-const socketServer = new Server(httpServer);
-
-socketServer.on('connection', async socket => {
-    
-    socket.emit('listProducts', await manager.getProducts());
-
-    socket.on('delete_event', async data =>{
-        await manager.deleteProduct(parseInt(data))
-        socket.emit('listProducts', await manager.getProducts());
-    })
-
-    socket.on('add_event', async data =>{
-        await manager.getProducts()
-        await manager.addProduct(data)
-        socket.emit('listProducts', await manager.getProducts());
-    })
-})
