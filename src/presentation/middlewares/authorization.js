be6adcee2589e8efc5fe478717bc5cc0055426da
Augusto Fernandez <augusfernandez23@gmail.com>
2023-08-05 15:40:59 -0300

    Se solucionó error que impedía la correcta validación de usuarios

--- src/presentation/middlewares/authorization.js
+++ src/presentation/middlewares/authorization.js
@@ -1,13 +1,20 @@
-import RoleManager from "../../domain/managers/RoleManager.js";
+import container from "../../container.js";
 
 const authorization = (permission) => {
     return async (req, res, next) => {
         const { _doc } = req.user;
-        const roleManager = new RoleManager()
 
-        const roles = await roleManager.getOne(_doc.role);
+        const roleRepository = await container.resolve('RoleRepository');
+        const validateRole = await roleRepository.validateId(_doc.role[0])
 
-        if (!_doc.isAdmin && !roles.id.permissions == permission) {
+        if ( _doc.isAdmin!==true && !validateRole ) {
+            return res.status(401).send({ message: 'Not authorized' });
+        }
+
+        const getRole = await roleRepository.getOne(_doc.role[0])
+        const findRole = getRole.id.permissions.map(role => role === permission)
+
+        if ( !findRole ) {
             return res.status(401).send({ message: 'Not authorized' });
         }
 
