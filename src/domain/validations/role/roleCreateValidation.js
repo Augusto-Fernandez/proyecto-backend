b04be6bffb345a42707bb98ec50dda0cd52de64d
Augusto Fernandez <augusfernandez23@gmail.com>
2023-09-02 09:54:27 -0300

    Se modificó la validación de role

--- src/domain/validations/role/roleCreateValidation.js
+++ src/domain/validations/role/roleCreateValidation.js
@@ -2,7 +2,7 @@ import z from 'zod';
 
 const roleCreateValidation = z.object({
     name: z.string().min(3).max(15),
-    permissions: z.string().array()/*.length(0)*/
+    permissions: z.string().array(z.string()).nonempty()
 });
 
 export default roleCreateValidation;
